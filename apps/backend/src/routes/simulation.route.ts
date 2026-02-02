ee2680ec5f5b52ef8a76cc5869340b190415d342
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2026-02-02 19:55:30 +0530

    remove siulation route and controller

--- apps/backend/src/routes/simulation.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleSimulation } from "../controllers/simulation.controller";
-
-const router = Router();
-
-router.get("/", handleSimulation);
-
-export default router;
